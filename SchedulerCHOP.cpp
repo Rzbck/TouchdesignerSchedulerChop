8db2d064273e19cce5525cd8a90e592e3336808f
Data_C0re_ <chauvel.charlie@gmail.com>
2025-01-10 18:59:42 +0100

    Add files via upload

--- SchedulerCHOP.cpp
+++ SchedulerCHOP.cpp
@@ -26,12 +26,12 @@ extern "C" {
     {
         delete (SchedulerCHOP*)instance;
     }
-};
+}
 
 SchedulerCHOP::SchedulerCHOP(const OP_NodeInfo* info)
-    : myNodeInfo(info), globalMode(false), interval(60.0), lastTriggerTime(0.0)
+    : myNodeInfo(info), globalMode(false), interval(60.0), lastTriggerTime(-1.0)
 {
-    activeDays.fill(true);
+    activeDays.fill(false);
 }
 
 SchedulerCHOP::~SchedulerCHOP() {}
@@ -59,33 +59,14 @@ void SchedulerCHOP::execute(CHOP_Output* output, const OP_Inputs* inputs, void* 
     globalMode = inputs->getParInt("Globalmode");
     interval = inputs->getParDouble("Globalinterval");
 
-    // Désactiver les paramètres des jours individuels si le mode global est activé
-    const std::array<std::string, 7> days = { "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" };
+    const std::array<std::string, 7> days = { "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" };
 
-    for (const auto& day : days)
-    {
-        std::string enableParamName = day + "enable";
-        std::string startHourParamName = day + "starthour";
-        std::string startMinuteParamName = day + "startminute";
-        std::string startSecondParamName = day + "startsecond";
-        std::string endHourParamName = day + "endhour";
-        std::string endMinuteParamName = day + "endminute";
-        std::string endSecondParamName = day + "endsecond";
-
-        inputs->enablePar(enableParamName.c_str(), !globalMode);
-        inputs->enablePar(startHourParamName.c_str(), !globalMode);
-        inputs->enablePar(startMinuteParamName.c_str(), !globalMode);
-        inputs->enablePar(startSecondParamName.c_str(), !globalMode);
-        inputs->enablePar(endHourParamName.c_str(), !globalMode);
-        inputs->enablePar(endMinuteParamName.c_str(), !globalMode);
-        inputs->enablePar(endSecondParamName.c_str(), !globalMode);
-    }
-
+    // Récupérer l'heure et la date courantes
     std::time_t currentTime = std::time(nullptr);
     std::tm localTime;
     localtime_s(&localTime, &currentTime);
 
-    int currentDay = localTime.tm_wday;
+    int currentDay = localTime.tm_wday; // Jour actuel (0 = Dimanche)
     double currentSeconds = localTime.tm_hour * 3600 + localTime.tm_min * 60 + localTime.tm_sec;
 
     bool isActiveDay = false;
@@ -93,6 +74,7 @@ void SchedulerCHOP::execute(CHOP_Output* output, const OP_Inputs* inputs, void* 
 
     if (globalMode)
     {
+        // Mode global
         startSeconds = inputs->getParInt("Globalstarthour") * 3600 +
             inputs->getParInt("Globalstartminute") * 60 +
             inputs->getParInt("Globalstartsecond");
@@ -101,11 +83,14 @@ void SchedulerCHOP::execute(CHOP_Output* output, const OP_Inputs* inputs, void* 
             inputs->getParInt("Globalendminute") * 60 +
             inputs->getParInt("Globalendsecond");
 
-        isActiveDay = true;
+        isActiveDay = true; // Toujours actif en mode global
     }
     else
     {
-        const std::string currentDayName = days[currentDay];
+        // Mode spécifique au jour
+        const std::string currentDayName = days[(currentDay + 6) % 7];
+
+
         if (inputs->getParInt((currentDayName + "enable").c_str()))
         {
             startSeconds = inputs->getParInt((currentDayName + "starthour").c_str()) * 3600 +
@@ -120,16 +105,32 @@ void SchedulerCHOP::execute(CHOP_Output* output, const OP_Inputs* inputs, void* 
         }
     }
 
+    // Vérification de la plage horaire active
     bool isInTimeRange = (currentSeconds >= startSeconds && currentSeconds <= endSeconds);
-    bool isIntervalElapsed = (currentSeconds - lastTriggerTime >= interval);
 
-    float triggerValue = (isActiveDay && isInTimeRange && isIntervalElapsed) ? 1.0f : 0.0f;
+    // Gestion de l'intervalle
+    float triggerValue = 0.0f;
 
-    if (triggerValue == 1.0f)
+    if (isActiveDay && isInTimeRange)
     {
-        lastTriggerTime = currentSeconds;
+        if (interval == 0.0)
+        {
+            // Si l'intervalle est 0, déclenche et reste à 1
+            triggerValue = 1.0f;
+        }
+        else
+        {
+            // Basé sur l'intervalle
+            double elapsed = currentSeconds - startSeconds;
+            if (std::fmod(elapsed, interval) < 1.0 && currentSeconds != lastTriggerTime)
+            {
+                triggerValue = 1.0f;
+                lastTriggerTime = currentSeconds;
+            }
+        }
     }
 
+    // Remplir la sortie
     for (int i = 0; i < output->numSamples; ++i)
     {
         output->channels[0][i] = triggerValue;
@@ -140,6 +141,7 @@ void SchedulerCHOP::setupParameters(OP_ParameterManager* manager, void* reserved
 {
     OP_NumericParameter np;
 
+    // Paramètres globaux
     np.page = "Global";
 
     np.name = "Globalmode";
@@ -174,46 +176,38 @@ void SchedulerCHOP::setupParameters(OP_ParameterManager* manager, void* reserved
     np.label = "Interval";
     manager->appendFloat(np);
 
-    const std::array<std::string, 7> days = { "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" };
+    // Paramètres par jour
+    const std::array<std::string, 7> days = { "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" };
 
     for (const auto& day : days)
     {
         np.page = day.c_str();
 
-        std::string paramName;
-
-        paramName = day + "enable";
-        np.name = paramName.c_str();
+        np.name = (day + "enable").c_str();
         np.label = "Enable";
         manager->appendToggle(np);
 
-        paramName = day + "starthour";
-        np.name = paramName.c_str();
+        np.name = (day + "starthour").c_str();
         np.label = "Start Hour";
         manager->appendInt(np);
 
-        paramName = day + "startminute";
-        np.name = paramName.c_str();
+        np.name = (day + "startminute").c_str();
         np.label = "Start Minute";
         manager->appendInt(np);
 
-        paramName = day + "startsecond";
-        np.name = paramName.c_str();
+        np.name = (day + "startsecond").c_str();
         np.label = "Start Second";
         manager->appendInt(np);
 
-        paramName = day + "endhour";
-        np.name = paramName.c_str();
+        np.name = (day + "endhour").c_str();
         np.label = "End Hour";
         manager->appendInt(np);
 
-        paramName = day + "endminute";
-        np.name = paramName.c_str();
+        np.name = (day + "endminute").c_str();
         np.label = "End Minute";
         manager->appendInt(np);
 
-        paramName = day + "endsecond";
-        np.name = paramName.c_str();
+        np.name = (day + "endsecond").c_str();
         np.label = "End Second";
         manager->appendInt(np);
     }
